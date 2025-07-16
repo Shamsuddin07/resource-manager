e038fc6cafd69b1068f18a6e5e20a326e8e71f87
vikram-dwarampudi <ext.adityavikram.dwarampudi@mastery.net>
2025-07-16 16:10:30 +0530

    addPod and other changes

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -9,12 +9,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { AddPodDialogComponent } from './add-pod-dialog/add-pod-dialog';
 
 @Component({
   selector: 'app-root',
   imports: [
     CommonModule, FormsModule, HttpClientModule, RouterOutlet,
-    MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDividerModule
+    MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatDividerModule,
+    MatDialogModule,
+    AddPodDialogComponent
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
